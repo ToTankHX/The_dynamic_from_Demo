8a2eef9914e44087c9db7c27f7b6243d01098784
1990-2 <a1990-2@1990-2deMini.lan>
2015-07-15 17:44:10 +0800

    Initial Commit

---
+++ The_dynamic_from_Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  The_dynamic_from_Demo
+//
+//  Created by 1990-2 on 15/7/15.
+//  Copyright (c) 2015年 1990-2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ The_dynamic_from_DemoTests/The_dynamic_from_DemoTests.m
@@ -0,0 +1,40 @@
+//
+//  The_dynamic_from_DemoTests.m
+//  The_dynamic_from_DemoTests
+//
+//  Created by 1990-2 on 15/7/15.
+//  Copyright (c) 2015年 1990-2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface The_dynamic_from_DemoTests : XCTestCase
+
+@end
+
+@implementation The_dynamic_from_DemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
